f8701c1fcd2ac9de87ebb37d6c858a971dd2d52b
yogi <yogi@gmail.cOm>
2023-02-11 21:03:34 +0530

    added code

---
+++ resources/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+    backend "azurerm" {
+        resource_group_name  = "terraform"
+        storage_account_name = "tfstorefile"
+        container_name       = "tfstate"
+        key                  = "rgterraform.tfstate"
+    }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "state-demo-secure" {
+  name     = "state-demo"
+  location = "eastus"
+}
+
