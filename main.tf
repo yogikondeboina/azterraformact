c6d1e3cbcbd2f9f5e9cb4f580b701d3902ebfc61
yogi <yogi@gmail.cOm>
2023-02-12 00:26:52 +0530

    RG

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "azurerm" {
 module "RG" {
 source = "./resourcegroup"
 location = var.location
-resoursource_group_name = var.resource_group_name  
+resource_group_name = var.resource_group_name  
 }
 
 # module "storage_account" {
