74ad6b125af0a96121e70c187b1e78438c25f567
yogi <yogi@gmail.cOm>
2023-02-12 20:54:32 +0530

    add storage

--- main.tf
+++ main.tf
@@ -23,9 +23,10 @@ provider "azurerm" {
 # }
 
 module "storage_account" {
-  source  = "./resources/storageaccount"
+  source  = "./storageaccount"
   resource_group_name   = var.resource_group_name
   location              = var.location
   storage_account_name  = var.azurerm_storage_account
+  storage_name          = var.azurerm_storage_account
 }
  
