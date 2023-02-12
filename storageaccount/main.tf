f0be2ad0b7bc8baea7b9bd4be29f6c9709a46c90
yogi <yogi@gmail.cOm>
2023-02-12 23:06:54 +0530

    remove st variable

--- storageaccount/main.tf
+++ storageaccount/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_container" "example" {
-  name                  = "yogiCNTR"
+  azurerm_storage_container_name   = "yogiCNTR"
   storage_account_name  = module.azure_storage_account.name
   container_access_type = "private"
 }
