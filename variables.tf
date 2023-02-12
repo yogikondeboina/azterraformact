5671a110975a5aa0c0f2d5e729af53113def7de0
yogi <yogi@gmail.cOm>
2023-02-13 00:03:44 +0530

    add storage

--- variables.tf
+++ variables.tf
@@ -1,18 +1,17 @@
+variable "storage_account_name" {
+ type = string  
+}
 
-# variable "resource_group_name" {
-#   type = string
-# }
+variable "azurerm_resource_group" {
+ type = string  
+}
+variable "location" {
+ type = string  
+}
 
-# variable "location" {
-#   type = string
-# }
-
-# variable "account_tier" {
-#   type = string
-#   default = "Standard"
-# }
-
-# variable "account_replication" {
-#   type = string
-#   default = "LRS"
-# }
+variable "account_tier" {
+ type   = string   
+}
+variable "account_replication_type" {
+ type = string  
+}
