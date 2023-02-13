f0137582ab832251b6794ed5a98da096d325afaf
yogi <yogi@gmail.cOm>
2023-02-13 16:04:18 +0530

    ST

--- main.tf
+++ main.tf
@@ -26,6 +26,6 @@ module "azure_storage_account" {
   source = "./storageaccount"
   storage_account_name     = var.storage_account_name
   location                  = var.location
-  azurerm_resource_group    = var.resource_group_name
+  resource_group_name   = var.resource_group_name
 }
  
