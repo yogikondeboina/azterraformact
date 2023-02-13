231b44778422862782bd36fa9f112863b7d28e35
yogi <yogi@gmail.cOm>
2023-02-13 23:51:15 +0530

    add storge

--- main.tf
+++ main.tf
@@ -37,6 +37,7 @@ module "app_service_plan" {
 
  module "app_service_app" {
   source =  "./resources/appservices"
+  app_service_name     = var.app_service_name
   app_service_plan_name = var.app_service_plan_name
   resource_group_name  = var.resource_group_name
   location             = var.location
