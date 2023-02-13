9d000e6063c4800d75a24cca03c0c6a333d6f150
yogi <yogi@gmail.cOm>
2023-02-13 23:46:08 +0530

    add storge

--- main.tf
+++ main.tf
@@ -31,6 +31,7 @@ provider "azurerm" {
 module "app_service_plan" {
   source = "./resources/appservices"
   app_service_plan_name = var.app_service_plan_name
+  app_service_name     = var.app_service_name
   resource_group_name  = var.resource_group_name
   location             = var.location
   }
