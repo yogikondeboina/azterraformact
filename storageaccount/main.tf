f61f59dee0a2e45b09e259f8a50ccadaab7527f3
yogi <yogi@gmail.cOm>
2023-02-13 16:38:42 +0530

    add st rg

--- storageaccount/main.tf
+++ storageaccount/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storageaccount" {
-  storage_account_name     = var.storage_account_name
+  name     = var.storage_account_name
   location                 = var.location
   resource_group_name      = var.resource_group_name
   account_tier             = "standerd"
