1b96456d295f34024a32a7cfc7772efadcfa22dc
yogi <yogi@gmail.cOm>
2023-02-13 23:10:21 +0530

    storage

--- resources/appservices/main.tf
+++ resources/appservices/main.tf
@@ -1,13 +1,13 @@
 resource "azurerm_app_service_plan" "appserveice_plan" {
   name                = var.app_service_plan_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  os_type             = "Linux"
-  sku_name            = "S1"
+  resource_group_name  = var.resource_group_name
+  location             = var.location
+  sku                  = var.app_service_plan_sku
 }
 
 resource "azurerm_app_service" "appserveice" {
   name                = var.app_service_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  }
+ resource_group_name  = var.resource_group_name
+  location             = var.location
+  app_service_plan_name = var.app_service_name
+}
