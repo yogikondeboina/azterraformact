209c7c598584e194164b55ba635c6c33f0093f18
yogi <yogi@gmail.cOm>
2023-02-13 21:57:01 +0530

    remove location

--- variables.tf
+++ variables.tf
@@ -9,9 +9,9 @@ variable "location" {
  type = string  
 }
 
-variable "app_service_plan_name" {
- type = string  
-}
+# variable "app_service_plan_name" {
+#  type = string  
+# }
 variable "app_service_name" {
  type = string  
 }
