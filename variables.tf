25a4c4ca3e00bd4b2a7fd9801a9096664d704ea2
yogi <yogi@gmail.cOm>
2023-02-14 22:55:35 +0530

    adf

--- variables.tf
+++ variables.tf
@@ -23,13 +23,6 @@ variable "name" {
   type = string
 }
 
-variable "location" {
-  type = string
-}
-
-variable "resource_group_name" {
-  type = string
-}
 
 variable "tags" {
   type = map(string)
