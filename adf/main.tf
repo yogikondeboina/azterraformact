b9324b423181e3f512bb6fae5cd4c1ffac4330fc
yogi <yogi@gmail.cOm>
2023-02-15 11:36:43 +0530

    change varsion adf provider

--- adf/main.tf
+++ adf/main.tf
@@ -1,5 +1,5 @@
 module "data_factory" {
- source    =  "./resources/adf"  
+ source    =  "./resources/datafactory"  
  location             = var.location
  resourcr_group_name   = var.resource_group_name
  adf_name              = var.ADFNAME
